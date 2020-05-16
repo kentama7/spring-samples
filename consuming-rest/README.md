c40889f27b81e6c4fb7d1e075294b8ba13582489
Kentama7 <kentama.7m@gmail.com>
2020-05-16 15:27:47 +0900

    Add consuming rest

---
+++ consuming-rest/README.md
@@ -0,0 +1,8 @@
+RESTful Web サービスの使用
+
+https://spring.pleiades.io/guides/gs/consuming-rest/
+
+---
+
+Spring Bootの引用文を返すAPI  
+https://gturnquist-quoters.cfapps.io/api/random
---
+++ consuming-rest/src/main/kotlin/com/example/consumingrest/ConsumingRestApplication.kt
@@ -0,0 +1,30 @@
+package com.example.consumingrest
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
+
+@SpringBootApplication
+class ConsumingRestApplication {
+    companion object {
+        private val log: Logger = LoggerFactory.getLogger(ConsumingRestApplication::class.java)
+    }
+
+    @Bean
+    fun restTemplate(builder: RestTemplateBuilder): RestTemplate = builder.build()
+
+    @Bean
+    fun run(restTemplate: RestTemplate): CommandLineRunner = CommandLineRunner {
+        val quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote::class.java)
+        log.info(quote.toString())
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<ConsumingRestApplication>(*args)
+}
